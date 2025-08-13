c753d37b5a08f611c0f2e1884fbf17a4bb6d6a10
Tobi <tobiloba.oyedele05@gmail.com>
2025-08-13 22:26:09 +0100

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import Main from "./components/common/Main";
-import Login from "./components/Login";
 import Home from "./components/Home";
 
 const App = () => {
   return (
     <div>
-      {/* <Login /> */}
-      {/* <Main /> */}
       <Home />
     </div>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,7 @@
 import Navbar from "./common/Navbar";
 import Main from "./common/Main";
 import ProductsSection from "./common/ProductsSection";
-import Zx9speaker from "./common/Zx9speaker";
 import BestAudioGear from "./common/BestAudioGear";
-import Footer from "./common/Footer";
 
 const Home = () => {
   return (
@@ -11,11 +9,9 @@ const Home = () => {
       <Navbar />
       <Main />
       <ProductsSection />
-      {/* <Zx9speaker /> */}
       <div className="mt-20">
         <BestAudioGear />
       </div>
-      {/* <Footer /> */}
     </div>
   );
 };
